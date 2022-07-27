4df40fcaf40c9774f4eaaacb1e0b8fdd9f7f9766
daniel Zamora <dzamora@capmotion.net>
2022-07-27 09:14:24 -0500

    feat(uikitLibModule import CommonModule)

--- projects/ui-kit-lib/src/lib/ui-kit-lib.module.ts
+++ projects/ui-kit-lib/src/lib/ui-kit-lib.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ButtonComponent } from './button/button.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [MaterialModule],
+  imports: [MaterialModule, CommonModule],
   exports: [ButtonComponent, MaterialModule],
 })
 export class UiKitLibModule {}
