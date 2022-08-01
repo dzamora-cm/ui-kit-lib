eeda3f162894985c0f33f08cddc95709cffa8b55
daniel Zamora <dzamora@capmotion.net>
2022-08-01 13:03:10 -0500

    add reactive forms

--- projects/ui-kit-lib/src/lib/ui-kit-lib.module.ts
+++ projects/ui-kit-lib/src/lib/ui-kit-lib.module.ts
@@ -5,10 +5,16 @@ import { ButtonComponent } from './button/button.component';
 import { MaterialModule } from './material.module';
 import { TagComponent } from './tag/tag.component';
 import { InputTextComponent } from './input-text/input-text.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ButtonComponent, TagComponent, InputTextComponent],
-  imports: [MaterialModule, CommonModule],
-  exports: [ButtonComponent, TagComponent, InputTextComponent],
+  imports: [MaterialModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    ButtonComponent,
+    TagComponent,
+    InputTextComponent,
+    ReactiveFormsModule,
+  ],
 })
 export class UiKitLibModule {}
